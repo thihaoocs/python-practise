f2f5f531e9e0ad9a44231eecd418cf7dd3bb9109
Thiha Oo <thihaoo@cloudsource.co.jp>
2022-11-23 10:29:39 +0000

    2nd commit

--- decorator/main.py
+++ decorator/main.py
@@ -2,6 +2,6 @@ import tank
 
 myTank = tank.Original_tank()
 
-myTank = tank.ERA(myTank)
+# myTank = tank.ERA(myTank)
 myTank = tank.Main_Battery(myTank)
 print("Upgraded Part: {0}, Damage {1}, Strength {2}".format(myTank.get_upgraded_part(),myTank.get_damage(),myTank.get_strength()))
