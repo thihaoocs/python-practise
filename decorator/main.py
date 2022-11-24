09c1279891c66b5f54ef918735f89822fc67c236
Thiha Oo <thihaoo@cloudsource.co.jp>
2022-11-24 05:29:24 +0000

    File names

--- decorator/main.py
+++ decorator/main.py
@@ -1,4 +1,4 @@
-import tank
+import class_decorator_tank as tank
 
 myTank = tank.Original_tank()
 print("Upgraded Part: {0}, Damage {1}, Strength {2}".format(myTank.get_upgraded_part(),myTank.get_damage(),myTank.get_strength()))
